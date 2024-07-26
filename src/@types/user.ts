3d1ecee3afc8e1531a09e59cbbb0aeab05f245ce
Caio Lima <caiojlimah@gmail.com>
2024-07-25 22:01:56 -0300

    [FEAT]: Zod Implementation

--- src/@types/user.ts
+++ src/@types/user.ts
@@ -1,10 +1,21 @@
+import { z, ZodType } from "zod";
+
 export interface IUser {
     name: string;
     document: string;
     email: string;
-    phone: number;
+    phone: string;
     address: string;
   }
+  
+  export const UserSchema: ZodType<IUser> = z.object({
+    name: z.string().min(1, "Nome é obrigatório"),
+    document: z.string().regex(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/, "CPF Inválido").min(1),
+    email: z.string().email({ message: "Email Inválido" }).min(1),
+    phone: z.string().regex(/^\([1-9]{2}\) (?:[2-8]|9[0-9])[0-9]{3}\-[0-9]{4}$/, "Telefone inválido").min(1),
+    address: z.string(),
+  });
+
   export type UserContextType = {
     users: IUser[];
     saveUser: (user: IUser) => void;
--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -1,24 +1,45 @@
 import * as React from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
-import { IUser } from '../@types/user';
+import { IUser, UserSchema } from '../@types/user';
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const UserForm: React.FC = () => {
     const {
         register,
         handleSubmit,
-      } = useForm<IUser>()
+        formState: { errors }
+      } = useForm<IUser>({ resolver: zodResolver(UserSchema) })
 
     const onSubmit: SubmitHandler<IUser> = (data) => console.log(data)
 
     return (
         /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
         <form onSubmit={handleSubmit(onSubmit)}>
-          {/* register your input into the hook by invoking the "register" function */}
-          <input {...register("name")} />
-          <input {...register("document")} />
-          <input {...register("email")} />
-          <input {...register("phone")} />
-          <input {...register("address")} />
+          <div>
+            <label htmlFor="name">Nome</label>
+            <input {...register("name")} />
+            {errors.name && <span>{errors.name.message}</span>}
+          </div>
+          <div>
+            <label htmlFor="document">CPF</label>
+            <input {...register("document")} />
+            {errors.document && <span>{errors.document.message}</span>}
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input {...register("email")} />
+            {errors.email && <span>{errors.email.message}</span>}
+          </div>
+          <div>
+            <label htmlFor="phone">Telefone</label>
+            <input {...register("phone")} />
+            {errors.phone && <span>{errors.phone.message}</span>}
+          </div>
+          <div>
+            <label htmlFor="address">Endereço</label>
+            <input {...register("address")} />
+            {errors.address && <span>{errors.address.message}</span>}
+          </div>
           <input type="submit" />
         </form>
       )
